cb7a67be7587bcce71d21eeb71124d21fc24bda2
Mackenna L. Wood <64872115+woodml@users.noreply.github.com>
2024-10-30 10:26:30 -0400

    Update and rename reducing.py to example.py
    Changed the name to be more descriptive.
    Added some descriptive comments and rearranged the code for clarity.

--- example.py
+++ example.py
@@ -19,59 +19,8 @@ print(folder)
 rp.clean_folder(folder)
 wv, spec, header = rp.reduce_and_extract(folder, target, 'dome1200', arc_id='97') # , exclude=['20230201/0114_30Gaia5294658.fits'])
 
-# out_file = '/Users/woodml/Observing/Reduced/' + folder + '/' + target + '_rvspectra.fits'
-# hdul = fits.open(out_file)
-# data = hdul['SPECTRUM'].data
-# wv = [x[0] for x in data]
-# spec = [x[1] for x in data]
-
-# Save plots
-
-# Full Spectrum
-out_file = '/Users/woodml/Observing/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra.pdf'
-plt.figure(figsize=(10, 4))
-plt.step(wv, spec)
-# plt.vlines([6562.8, 6708, 7126], 0, 500, color='k', ls='--')
-plt.xlim(6250, 7550)
-plt.xlabel('wavelength (\u212B)')
-plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
-
-# Halpha
-out_file = '/Users/woodml/Observing/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_ha.pdf'
-plt.figure(figsize=(10, 4))
-plt.plot(wv, spec)
-plt.minorticks_on()
-plt.vlines([6562.8, 6708, 7126], 0, 3050, color='k', ls='--')
-plt.xlim(6500, 6600)
-# plt.ylim(0, 4050)
-plt.xlabel('wavelength (\u212B)')
-plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
-
-# Li
-out_file = '/Users/woodml/Observing/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_li.pdf'
-plt.figure(figsize=(10,4))
-plt.plot(wv, spec)
-plt.vlines([6562.8, 6708, 7126], 0, 1000, color='k', ls='--')
-plt.minorticks_on()
-plt.xlim(6690, 6720)
-# plt.ylim(0, 200)
-plt.xlabel('wavelength (\u212B)')
-plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
-
-# TiO
-out_file = '/Users/woodml/Observing/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_tio.pdf'
-plt.figure(figsize=(10, 4))
-plt.plot(wv, spec)
-plt.vlines([6562.8, 6708, 7126], 0, 1000, color='k', ls='--')
-plt.minorticks_on()
-plt.xlim(7050, 7150)
-# plt.ylim(0, 1000)
-plt.xlabel('wavelength (\u212B)')
-plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
-plt.show()
-
-# Save data
-out_file = '/Users/woodml/Observing/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra.fits'
+# Save the spectrum to a fits file
+out_file = '~SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra.fits'
 # spectrum
 hdu_spec = fits.BinTableHDU.from_columns([fits.Column(name='wv(Ang)', format='E', array=wv), fits.Column(name='Counts', format='E', array=spec)])
 hdu_spec.name = 'SPECTRUM'
@@ -83,3 +32,47 @@ hdul = fits.HDUList([empty_primary, hdu_spec])
 print('\nMy Structure Info:\n')
 hdul.info()
 hdul.writeto(out_file, overwrite=True)
+
+# Create plots of various regions of the spectrum
+# Full Spectrum
+out_file = '~/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra.pdf'
+plt.figure(figsize=(10, 4))
+plt.step(wv, spec)
+# plt.vlines([6562.8, 6708, 7126], 0, 500, color='k', ls='--')
+plt.xlim(6250, 7550)
+plt.xlabel('wavelength (\u212B)')
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
+
+# Halpha
+out_file = '~/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_ha.pdf'
+plt.figure(figsize=(10, 4))
+plt.plot(wv, spec)
+plt.minorticks_on()
+plt.vlines([6562.8, 6708, 7126], 0, 3050, color='k', ls='--')
+plt.xlim(6500, 6600)
+# plt.ylim(0, 4050)
+plt.xlabel('wavelength (\u212B)')
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
+
+# Li
+out_file = '~/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_li.pdf'
+plt.figure(figsize=(10,4))
+plt.plot(wv, spec)
+plt.vlines([6562.8, 6708, 7126], 0, 1000, color='k', ls='--')
+plt.minorticks_on()
+plt.xlim(6690, 6720)
+# plt.ylim(0, 200)
+plt.xlabel('wavelength (\u212B)')
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
+
+# TiO
+out_file = '~/SOAR:Goodman/Reduced/' + date + '/' + target + '_spectra_tio.pdf'
+plt.figure(figsize=(10, 4))
+plt.plot(wv, spec)
+plt.vlines([6562.8, 6708, 7126], 0, 1000, color='k', ls='--')
+plt.minorticks_on()
+plt.xlim(7050, 7150)
+# plt.ylim(0, 1000)
+plt.xlabel('wavelength (\u212B)')
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2)
+plt.show()
